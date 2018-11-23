b28b53c7af89bf1fedb1f6ea808e8e0db1ad268f
Pedro Acosta <pacosta@asesoria.com.py>
2018-11-23 10:58:07 -0300

    se agrega model upload

---
+++ app/Upload.php
@@ -0,0 +1,20 @@
+<?php
+
+// Upload.php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Upload extends Model
+{
+
+  protected $fillable = [
+    'filename'
+  ];
+
+    public function user()
+    {
+      return $this->belongsTo(User::class);
+    }
+}
